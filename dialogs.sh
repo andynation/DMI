ea140a4c1084a7b9ebcce5540f4e90883c49b2c6
aa13.a.klints@pt.edu.lv <anddddreyy@gmail.com>
2017-10-18 12:03:54 +0300

    20171018_12_00

---
+++ dialogs.sh
@@ -0,0 +1,14 @@
+#1/bin/bash
+
+echo "Cienijamais lietotaaj, luudzu, ievadi pirmo skaitli"
+read a
+echo "Ceinijamais lietotaj, luudzu, ievadi otro skaitli"
+read b
+echo "cienijamais lietotaj, luudzu, ievadi trešo skaitli"
+#if [ $a -gt $b ]
+if (( $a > $b ))
+then
+echo "a ($a) > b ($b)"
+else
+echo "a ($a) < b ($b)"
+fi 
